2a500ee0dc36055d4247df0455ea652e8eb9689f
Michael Sandrof <msandrof@jamcity.com>
2021-07-23 14:05:02 -0700

    Fixed check for unity project root. Fixed repo to pull packages from.

--- common-sdk-install.sh
+++ common-sdk-install.sh
@@ -2,14 +2,14 @@
 
 command -v jq >/dev/null >/dev/null || brew install jq
 
-if [[ ! -d "Assets/Plugins" ]]; then
+if [[ ! -d "Assets" ]] || [[ ! -d "ProjectSettings" ]]; then
   echo "You must run this from the root of your Unity project."
   exit 1
 fi
 
 function fetch () {
     package=$1
-    latest=$(curl --silent -H "X-Result-Detail: info " "https://artifactory.sgn.com:443/artifactory/api/search/artifact?name=${package}.zip&repos=libs-release-local" | jq -r "(.results|=sort_by(.created)[-1].downloadUri).results")
+    latest=$(curl --silent -H "X-Result-Detail: info " "https://artifactory.sgn.com:443/artifactory/api/search/artifact?name=${package}.zip&repos=jcpm-release-local" | jq -r "(.results|=sort_by(.created)[-1].downloadUri).results")
     curl --silent -o /tmp/package.zip ${latest}
     rm -rf ${package}
     unzip -q /tmp/package.zip
